125ad6dd79a47a5f9fc446d59cec51ff75b0f305
Pei Meng <2398696309@qq.com>
2025-02-18 19:09:48 +0800

    tools: create display panes

---
+++ create_display_panes.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+tmux new-window -n 'Arm CCA'
+
+tmux split-window -v
+tmux split-window -h -t 0
+tmux split-window -h -t 1
+
+if [ ! -d $HOME/.tmux ]; then
+  tmux send-keys -t 0 'socat -,rawer TCP-LISTEN:54320' C-m
+  tmux send-keys -t 1 'socat -,rawer TCP-LISTEN:54321' C-m
+  tmux send-keys -t 2 'socat -,rawer TCP-LISTEN:54322' C-m
+  tmux send-keys -t 3 'socat -,rawer TCP-LISTEN:54323' C-m
+else
+  tmux send-keys -t 1 'socat -,rawer TCP-LISTEN:54320' C-m
+  tmux send-keys -t 2 'socat -,rawer TCP-LISTEN:54321' C-m
+  tmux send-keys -t 3 'socat -,rawer TCP-LISTEN:54322' C-m
+  tmux send-keys -t 4 'socat -,rawer TCP-LISTEN:54323' C-m
+fi
+
+tmux select-window -l
