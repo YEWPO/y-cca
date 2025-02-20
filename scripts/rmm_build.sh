3ea460f258fdf1d10efcd0ef79ade5d049733e9d
Pei Meng <2398696309@qq.com>
2025-02-20 16:19:55 +0800

    chore(rmm): Add CMAKE_EXPORT_COMPILE_COMMANDS

--- scripts/rmm_build.sh
+++ scripts/rmm_build.sh
@@ -5,6 +5,6 @@ RMM_DIR=${ROOT_DIR}/rmm
 
 cd $RMM_DIR
 export CROSS_COMPILE=aarch64-linux-gnu-
-cmake -DCMAKE_BUILD_TYPE=Debug -DRMM_CONFIG=qemu_sbsa_defcfg -B build-sbsa
-cmake --build build-sbsa
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DRMM_CONFIG=qemu_sbsa_defcfg -B build
+cmake --build build
 cd $ROOT_DIR
