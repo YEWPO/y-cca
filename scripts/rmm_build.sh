24df7be8bc74e3f852657fd12501632532829d6e
Pei Meng <2398696309@qq.com>
2025-02-21 11:41:59 +0800

    fix(rmm): check build status

--- scripts/rmm_build.sh
+++ scripts/rmm_build.sh
@@ -5,10 +5,15 @@ RMM_DIR=${ROOT_DIR}/rmm
 
 cd $RMM_DIR
 export CROSS_COMPILE=aarch64-linux-gnu-
-
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -DCMAKE_BUILD_TYPE=Debug -DRMM_CONFIG=qemu_sbsa_defcfg \
   -DRMM_V1_1=$1 \
   -B build
+
 cmake --build build
+
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
 cd $ROOT_DIR
