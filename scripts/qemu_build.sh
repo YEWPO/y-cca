72fd548302f1767d347a11f681c1d514a289444d
Pei Meng <2398696309@qq.com>
2025-02-24 11:54:02 +0800

    fix(qemu): check qemu build status

--- scripts/qemu_build.sh
+++ scripts/qemu_build.sh
@@ -6,4 +6,9 @@ QEMU_DIR=$ROOT_DIR/qemu
 cd $QEMU_DIR
 ./configure --target-list=aarch64-softmmu --enable-slirp --disable-docs
 make -j`nproc`
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 cd $ROOT_DIR
